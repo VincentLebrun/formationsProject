660ce8d541d51caf5abdb223ceeb29f72184977f
vincent <lebrunvincent12@gmail.com>
2022-05-04 12:15:11 +0200

    delete repo function too

--- src/main/java/fr/afpa/formations/repository/CenterRepository.java
+++ src/main/java/fr/afpa/formations/repository/CenterRepository.java
@@ -1,2 +1,13 @@
-package fr.afpa.formations.repository;public interface CenterRepository {
+package fr.afpa.formations.repository;
+
+import fr.afpa.formations.model.Center;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CenterRepository extends JpaRepository<Center, Long> {
+
 }
