f41f98b32ba39ab89a2cd7ef5989b3a102c0e93d
vincent <lebrunvincent12@gmail.com>
2022-05-04 14:36:08 +0200

    Add save

--- src/main/java/fr/afpa/formations/service/CenterService.java
+++ src/main/java/fr/afpa/formations/service/CenterService.java
@@ -2,17 +2,18 @@ package fr.afpa.formations.service;
 
 import fr.afpa.formations.model.Center;
 import fr.afpa.formations.repository.CenterRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class CenterService {
 
     private final CenterRepository centerRepository;
+
+    public CenterService(CenterRepository centerRepository) {
+        this.centerRepository = centerRepository;
+    }
 
     public List<Center> getAll() {
         return centerRepository.findAll();
