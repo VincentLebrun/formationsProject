72c2058121763d16aebb1c1469f8c6613e11756a
vincent <lebrunvincent12@gmail.com>
2022-05-04 12:13:37 +0200

    remove some service useless because find all already exist in spring

--- src/main/java/fr/afpa/formations/service/CenterService.java
+++ src/main/java/fr/afpa/formations/service/CenterService.java
@@ -1,2 +1,24 @@
-package fr.afpa.formations.service;public class CenterService {
+package fr.afpa.formations.service;
+
+import fr.afpa.formations.model.Center;
+import fr.afpa.formations.repository.CenterRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CenterService {
+
+    private final CenterRepository centerRepository;
+
+    public List<Center> getAll() {
+        return centerRepository.findAll();
+    }
+
+    public void save(Center center) {
+        centerRepository.save(center);
+    }
 }
