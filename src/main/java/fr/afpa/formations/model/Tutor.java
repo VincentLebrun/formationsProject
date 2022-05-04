d07e1f423f91aad0a18797dfbddfbb4cac2578e8
vincent <lebrunvincent12@gmail.com>
2022-05-04 09:37:53 +0200

    add relation ManyToOne tutor to module

--- src/main/java/fr/afpa/formations/model/Tutor.java
+++ src/main/java/fr/afpa/formations/model/Tutor.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.sql.Date;
-import java.util.List;
 
 
 @Entity
@@ -17,10 +16,14 @@ public class Tutor {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutor_seq")
     @SequenceGenerator(name = "tutor_seq", initialValue = 1000, allocationSize = 1)
     private Long id;
+
     private String name;
     private String surname;
     private Date birthDate;
     public String mail;
     public Date startDate;
+    @ManyToOne
+    @JoinColumn(name = "form_module")
+    private Module formModule;
 
 }
