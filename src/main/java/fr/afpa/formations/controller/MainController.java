3c9a6bddd3b08b7bdc12d0c23c862ff56749508d
vincent <lebrunvincent12@gmail.com>
2022-05-04 12:11:35 +0200

    main juste serve to reference '/ '

--- src/main/java/fr/afpa/formations/controller/MainController.java
+++ src/main/java/fr/afpa/formations/controller/MainController.java
@@ -1,2 +1,17 @@
-package fr.afpa.formations.controller;public class MainController {
+package fr.afpa.formations.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    //just to have main
+    @GetMapping("/")
+    public String index(Model model) {
+        String name = "Vincent";
+        model.addAttribute("name" , name);
+        return "index";
+    }
+
 }
