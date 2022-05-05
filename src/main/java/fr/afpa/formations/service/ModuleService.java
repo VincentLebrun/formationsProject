05c3f27757b7517aa0154928ed5b3e21740b6033
vincent <lebrunvincent12@gmail.com>
2022-05-05 14:45:24 +0200

    add command save get all and get module by id

--- src/main/java/fr/afpa/formations/service/ModuleService.java
+++ src/main/java/fr/afpa/formations/service/ModuleService.java
@@ -1,2 +1,22 @@
-package fr.afpa.formations.service;public class ModuleService {
+package fr.afpa.formations.service;
+
+import fr.afpa.formations.model.Module;
+import fr.afpa.formations.repository.ModuleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ModuleService {
+    private final ModuleRepository moduleRepository;
+
+    public List<Module> getAllModule() {
+        return moduleRepository.findAll();
+    }
+    public void saveModule(Module module){
+        moduleRepository.save(module);
+    }
+
 }
