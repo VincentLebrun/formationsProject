c4831380744b4ff4e12c4a315e48a9dd2ea38648
vincent <lebrunvincent12@gmail.com>
2022-05-05 14:35:18 +0200

    Finished controller post get and id

--- src/main/java/fr/afpa/formations/controller/CenterController.java
+++ src/main/java/fr/afpa/formations/controller/CenterController.java
@@ -9,20 +9,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Slf4j
 @Controller
 public class CenterController {
-
-
     private final CenterService centerService;
 
     public CenterController(CenterService centerService) {
         this.centerService = centerService;
     }
-
 
     @GetMapping("/center")
     public String findCenter(Model model) {
@@ -35,14 +33,20 @@ public class CenterController {
     public String addCenter(@ModelAttribute Center center) {
         centerService.save(center);
         return "redirect:/center";
-
     }
 
     @GetMapping("/center-form")
     public String formCenterPage(Model model) {
         Center center = new Center();
-        model.addAttribute("center" , center);
+        model.addAttribute("center", center);
         return "formCenter";
+    }
+
+    @GetMapping("center-detail")
+    public String getCenterById(Model model, @RequestParam Long id) {
+        Center center = centerService.getCenterById(id);
+        model.addAttribute("center" , center);
+        return "showCenterId";
     }
 
 }
