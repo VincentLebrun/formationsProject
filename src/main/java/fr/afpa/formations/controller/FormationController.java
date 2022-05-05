7316c2aadeb820ff48f7db7ed9ae6c5d9965fe7a
vincent <lebrunvincent12@gmail.com>
2022-05-05 14:36:16 +0200

    add post get and get by id

--- src/main/java/fr/afpa/formations/controller/FormationController.java
+++ src/main/java/fr/afpa/formations/controller/FormationController.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,10 +16,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FormationController {
     private final FormationService formationService;
+
     @GetMapping("/formation")
     public String formationList(Model model) {
         List<Formation> formations = formationService.getAll();
-        model.addAttribute("formations" , formations);
-        return "";
+        model.addAttribute("formations", formations);
+        return "showFormation";
     }
+
+    @GetMapping("/formation-form")
+    public String formationForm(Model model) {
+        Formation formation = new Formation();
+        model.addAttribute("formation", formation);
+        return "formFormation";
+    }
+
+    @PostMapping("/add-formation")
+    public String addAddress(@ModelAttribute Formation formation) {
+        formationService.saveFormation(formation);
+        return "redirect:/center";
+    }
+
+    @GetMapping("/formationDetail")
+    public String getFormationById(Model model , @RequestParam Long id) {
+        Formation formation = formationService.getFormationById(id);
+        model.addAttribute("formations" , formation);
+        return "showFormationId";
+    }
+
 }
