46392e55f26b8b17e37bc020c0cbfb8ee50140f6
vincent <lebrunvincent12@gmail.com>
2022-05-05 14:39:55 +0200

    add post get and get by id

--- src/main/java/fr/afpa/formations/controller/ModuleController.java
+++ src/main/java/fr/afpa/formations/controller/ModuleController.java
@@ -1,2 +1,30 @@
-package fr.afpa.formations.controller;public class ModuleController {
+package fr.afpa.formations.controller;
+
+
+import fr.afpa.formations.model.Module;
+import fr.afpa.formations.service.ModuleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+
+
+@Controller
+@RequiredArgsConstructor
+public class ModuleController {
+    private final ModuleService moduleService;
+
+    @GetMapping("/module")
+    public String findModule(Model model) {
+        List<Module> modules = moduleService.getAllModule();
+        model.addAttribute("modules" , modules);
+        return "showModule";
+    }
+    @PostMapping("/")
+
+
 }
