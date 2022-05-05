378633437beba070350b1d2ece6db4a3db596bff
vincent <lebrunvincent12@gmail.com>
2022-05-05 14:40:55 +0200

    add @getter @Setter for lombok

--- src/main/java/fr/afpa/formations/model/Module.java
+++ src/main/java/fr/afpa/formations/model/Module.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Module {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "module_seq")
