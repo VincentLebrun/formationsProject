ad5943caca4139b22b7faaebc74b57815c99b281
vincent <lebrunvincent12@gmail.com>
2022-05-06 12:21:20 +0200

    Prepare change to be rest

--- src/main/java/fr/afpa/formations/controller/ModuleController.java
+++ src/main/java/fr/afpa/formations/controller/ModuleController.java
@@ -2,22 +2,20 @@ package fr.afpa.formations.controller;
 
 
 import fr.afpa.formations.model.Module;
+import fr.afpa.formations.repository.ModuleRepository;
 import fr.afpa.formations.service.ModuleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ModuleController {
     private final ModuleService moduleService;
-
+    private final ModuleRepository moduleRepository;
     @GetMapping("/module")
     public String findModule(Model model) {
         List<Module> modules = moduleService.getAllModule();
@@ -31,10 +29,10 @@ public class ModuleController {
         return "formModule";
     }
 
-    @PostMapping("/add-module")
-    public String addModule(@ModelAttribute Module module) {
-        moduleService.saveModule(module);
-        return "redirect:/center";
+    @PostMapping("/addModule")
+    public Module addModule(@RequestBody Module newModule) {
+     return moduleService.saveModule(newModule);
+
     }
 
 
