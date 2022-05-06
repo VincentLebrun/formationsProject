d72b80319f9d94799e598790cb8653deb82c41ee
vincent <lebrunvincent12@gmail.com>
2022-05-06 12:20:45 +0200

    Prepare change to be rest

--- src/main/java/fr/afpa/formations/controller/CenterController.java
+++ src/main/java/fr/afpa/formations/controller/CenterController.java
@@ -2,51 +2,47 @@ package fr.afpa.formations.controller;
 
 import fr.afpa.formations.model.Center;
 
+import fr.afpa.formations.repository.CenterRepository;
 import fr.afpa.formations.service.CenterService;
 import org.springframework.stereotype.Controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
+@RequestMapping("/center")
 public class CenterController {
     private final CenterService centerService;
+    private final CenterRepository centerRepository;
 
-    public CenterController(CenterService centerService) {
+
+    public CenterController(CenterService centerService, CenterRepository centerRepository) {
         this.centerService = centerService;
+        this.centerRepository = centerRepository;
     }
 
-    @GetMapping("/center")
-    public String findCenter(Model model) {
-        List<Center> centers = centerService.getAll();
-        model.addAttribute("centers", centers);
-        return "center";
+    @GetMapping("/all")
+    List<Center> findCenter() {
+
+        return centerService.getAllCenter();
+
     }
 
-    @PostMapping("/add-center")
-    public String addCenter(@ModelAttribute Center center) {
-        centerService.save(center);
-        return "redirect:/center";
+    @PostMapping("/add")
+    public Center addCenter(@RequestBody Center center) {
+        return centerService.saveCenter(center);
+        //attention toujours via les services !!
     }
 
-    @GetMapping("/center-form")
-    public String formCenterPage(Model model) {
-        Center center = new Center();
-        model.addAttribute("center", center);
-        return "formCenter";
-    }
+    /**
+    @PutMapping("/update")
+    public Center updateCenter(Model model, @RequestBody Center center) {
 
-    @GetMapping("center-detail")
-    public String getCenterById(Model model, @RequestParam Long id) {
-        Center center = centerService.getCenterById(id);
-        model.addAttribute("center" , center);
-        return "showCenterId";
     }
+    **/
+
 
 }
