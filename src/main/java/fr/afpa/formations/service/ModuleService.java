bc032f808a5ef03349c9d484bc0c3d7de4076b89
vincent <lebrunvincent12@gmail.com>
2022-05-06 16:08:04 +0200

    Create service for Module

--- src/main/java/fr/afpa/formations/service/ModuleService.java
+++ src/main/java/fr/afpa/formations/service/ModuleService.java
@@ -4,6 +4,8 @@ import fr.afpa.formations.model.Module;
 import fr.afpa.formations.repository.ModuleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,5 +20,9 @@ public class ModuleService {
     public Module saveModule(Module module){
        return moduleRepository.save(module);
     }
+    public void  deleteModuleById( Long id) {
+        moduleRepository.deleteById(id);
+    }
+    public Module
 
 }
