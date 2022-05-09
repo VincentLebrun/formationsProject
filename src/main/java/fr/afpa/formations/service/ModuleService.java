891fe454681624c82882b1602ccd4131c53d8c06
vincent <lebrunvincent12@gmail.com>
2022-05-09 16:07:01 +0200

    preparing to dto

--- src/main/java/fr/afpa/formations/service/ModuleService.java
+++ src/main/java/fr/afpa/formations/service/ModuleService.java
@@ -3,26 +3,36 @@ package fr.afpa.formations.service;
 import fr.afpa.formations.model.Module;
 import fr.afpa.formations.repository.ModuleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
 public class ModuleService {
-    private final ModuleRepository moduleRepository;
+    private final ModuleRepository  moduleRepository;
 
     public List<Module> getAllModule() {
         return moduleRepository.findAll();
     }
-    public Module saveModule(Module module){
-       return moduleRepository.save(module);
+
+    public Module saveModule(Module module) {
+        return moduleRepository.save(module);
     }
-    public void  deleteModuleById( Long id) {
+
+    public void deleteModuleById(Long id) {
         moduleRepository.deleteById(id);
     }
-    public Module
+
+    public Module getModuleById(Long id) {
+        return moduleRepository.findById(id).orElse(null);
+    }
+    public Module updateModuleId(Module module) {
+        return  moduleRepository.save(module);
+    }
+
 
 }
