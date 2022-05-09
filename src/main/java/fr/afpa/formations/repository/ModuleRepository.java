99ca249189f21b602cbe3e7ab6530cdc0bfd296b
vincent <lebrunvincent12@gmail.com>
2022-05-09 16:04:13 +0200

    module repository change does not work

--- src/main/java/fr/afpa/formations/repository/ModuleRepository.java
+++ src/main/java/fr/afpa/formations/repository/ModuleRepository.java
@@ -1,12 +1,13 @@
 package fr.afpa.formations.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import fr.afpa.formations.model.Module;
 @Repository
 public interface ModuleRepository extends JpaRepository<Module, Long> {
     /**
-   
     Module getById(Long aLong);
     **/
+
 }
