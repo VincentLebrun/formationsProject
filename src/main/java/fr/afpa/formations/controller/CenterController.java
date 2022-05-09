ba985ae7d63f42955b3ec503c572f5b97d6a3baa
vincent <lebrunvincent12@gmail.com>
2022-05-09 15:59:46 +0200

    Module controller update doesn't work return the object

--- src/main/java/fr/afpa/formations/controller/CenterController.java
+++ src/main/java/fr/afpa/formations/controller/CenterController.java
@@ -4,26 +4,21 @@ import fr.afpa.formations.model.Center;
 
 import fr.afpa.formations.repository.CenterRepository;
 import fr.afpa.formations.service.CenterService;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/center")
 public class CenterController {
     private final CenterService centerService;
     private final CenterRepository centerRepository;
 
-
-    public CenterController(CenterService centerService, CenterRepository centerRepository) {
-        this.centerService = centerService;
-        this.centerRepository = centerRepository;
-    }
 
     @GetMapping("/all")
     List<Center> findCenter() {
@@ -38,14 +33,8 @@ public class CenterController {
         //attention toujours via les services !!
     }
 
-    @PutMapping("update/{id}")
-    public Center updateCenter(Model model, @PathVariable Center center) {
-        Optional<Center> center1 = centerService.getCenterById(center.getId());
-        model.addAttribute("center" , center);
+    @PutMapping("/update/{id}")
+    public Center updateCenter(@PathVariable  @RequestBody Center center) {
         return centerService.saveCenter(center);
     }
-
-
-
-
 }
