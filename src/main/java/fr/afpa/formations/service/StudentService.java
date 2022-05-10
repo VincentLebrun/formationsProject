368b01fa3f4ada155d77c517574ce9622c0313aa
vincent <lebrunvincent12@gmail.com>
2022-05-10 11:50:17 +0200

    add all service for student finished

--- src/main/java/fr/afpa/formations/service/StudentService.java
+++ src/main/java/fr/afpa/formations/service/StudentService.java
@@ -1,2 +1,33 @@
-package fr.afpa.formations.service;public class StudentService {
+package fr.afpa.formations.service;
+
+import fr.afpa.formations.model.Student;
+import fr.afpa.formations.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+    private final StudentRepository studentRepository;
+
+
+    public List<Student> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    public Student saveStudent(Student student) {
+        return  studentRepository.save(student);
+
+    }
+
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public Student getStudentById(Long id) {
+       return  studentRepository.findById(id).get();
+    }
+
 }
