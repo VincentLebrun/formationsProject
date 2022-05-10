bd5ebef462875356f6457b2e259d527c8e4b660c
vincent <lebrunvincent12@gmail.com>
2022-05-10 11:38:56 +0200

    Added student controller  crud ok

--- src/main/java/fr/afpa/formations/controller/StudentController.java
+++ src/main/java/fr/afpa/formations/controller/StudentController.java
@@ -1,5 +1,46 @@
 package fr.afpa.formations.controller;
 
+
+import fr.afpa.formations.model.Student;
+import fr.afpa.formations.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping("list")
+    public List<Student> studentList() {
+        return studentService.getAllStudent();
+    }
+
+    @PostMapping("/add")
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudentById(id);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        Student currentStudent = studentService.getStudentById(id);
+        currentStudent.setBirthDate(student.getBirthDate());
+        currentStudent.setMail(student.getMail());
+        currentStudent.setName(student.getName());
+        currentStudent.setStartDate(student.getStartDate());
+        currentStudent.setSurname(student.getSurname());
+        currentStudent.setSession(student.getSession());
+        return ResponseEntity.ok(currentStudent);
+    }
+
 
 }
