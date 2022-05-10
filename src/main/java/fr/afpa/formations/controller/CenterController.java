80da6503bceae05a845276b42113063f5ac5c920
vincent <lebrunvincent12@gmail.com>
2022-05-10 11:38:09 +0200

    Update ok crud ok

--- src/main/java/fr/afpa/formations/controller/CenterController.java
+++ src/main/java/fr/afpa/formations/controller/CenterController.java
@@ -6,6 +6,7 @@ import fr.afpa.formations.repository.CenterRepository;
 import fr.afpa.formations.service.CenterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,8 +34,17 @@ public class CenterController {
         //attention toujours via les services !!
     }
 
+    @DeleteMapping("delete/{id}")
+    public void deleteCenter(@PathVariable Long id) {
+        centerService.deleteCenterById(id);
+    }
+
     @PutMapping("/update/{id}")
-    public Center updateCenter(@PathVariable  @RequestBody Center center) {
-        return centerService.saveCenter(center);
+    public ResponseEntity updateCenter(@PathVariable Long id, @RequestBody Center center) {
+        Center currentCenter = centerService.getCenterById(id);
+        currentCenter.setAddress(center.getAddress());
+        currentCenter.setForm(center.getForm());
+        currentCenter.setName(currentCenter.getName());
+        return ResponseEntity.ok(currentCenter);
     }
 }
