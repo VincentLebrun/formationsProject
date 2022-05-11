9b8eab4e22dd9283d2edc7f2c955cca0f4afd587
vincent <lebrunvincent12@gmail.com>
2022-05-11 15:38:44 +0200

    Add controller to make crud tutor

--- src/main/java/fr/afpa/formations/controller/TutorController.java
+++ src/main/java/fr/afpa/formations/controller/TutorController.java
@@ -1,2 +1,34 @@
-package fr.afpa.formations.controller;public class TutorController {
+package fr.afpa.formations.controller;
+
+import fr.afpa.formations.model.Tutor;
+import fr.afpa.formations.repository.TutorRepository;
+import fr.afpa.formations.service.TutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/tutor")
+public class TutorController {
+    private final TutorService tutorService;
+
+    @GetMapping("/")
+    List<Tutor> findTutor() {
+        return tutorService.getAllTutor();
+    }
+
+    @PostMapping("/")
+    public Tutor addTutor(@RequestBody Tutor tutor) {
+        return tutorService.saveTutor(tutor);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTutor(@PathVariable Long id) {
+        tutorService.getTutorById(id);
+    }
+    
+
 }
