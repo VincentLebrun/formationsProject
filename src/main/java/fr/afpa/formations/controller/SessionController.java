a17eb799388c24468b718426d58e8fe38ecdf624
vincent <lebrunvincent12@gmail.com>
2022-05-12 10:02:54 +0200

    move function update in to service work more properly

--- src/main/java/fr/afpa/formations/controller/SessionController.java
+++ src/main/java/fr/afpa/formations/controller/SessionController.java
@@ -3,7 +3,6 @@ package fr.afpa.formations.controller;
 import fr.afpa.formations.model.Session;
 import fr.afpa.formations.service.SessionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,32 +13,25 @@ import java.util.List;
 public class SessionController {
     private final SessionService sessionService;
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public List<Session> sessionList() {
         return sessionService.getAllSession();
     }
 
-    @PostMapping("/add")
+    @PostMapping("/")
     public Session addSession(@RequestBody Session session) {
         return sessionService.saveSession(session);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public void deleteSession(@PathVariable Long id) {
         sessionService.deleteSessionById(id);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity updateSession(@PathVariable Long id, @RequestBody Session session) {
-        Session currentSession = sessionService.getSessionById(id);
-        currentSession.setFinishDate(session.getFinishDate());
-        currentSession.setInternList(session.getInternList());
-        currentSession.setModuleList(session.getModuleList());
-        currentSession.setPlace(session.getPlace());
-        currentSession.setStartDate(session.getStartDate());
-        currentSession.setFormation(session.getFormation());
-        currentSession.setModuleList(session.getModuleList());
-        return  ResponseEntity.ok(currentSession);
+    @PutMapping("/{id}")
+    public Session updateSession(@PathVariable Long id, @RequestBody Session session) {
+         Session current = sessionService.updateSessions(session, id);
+        return  current;
     }
 
 
