29a15c2975834b53818295f79a50e94e8167d54a
vincent <lebrunvincent12@gmail.com>
2022-05-12 11:10:48 +0200

    Move update function formation in service

--- src/main/java/fr/afpa/formations/controller/FormationController.java
+++ src/main/java/fr/afpa/formations/controller/FormationController.java
@@ -4,6 +4,7 @@ import fr.afpa.formations.model.Formation;
 import fr.afpa.formations.service.FormationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,31 +15,36 @@ import java.util.List;
 public class FormationController {
     private final FormationService formationService;
 
-    @GetMapping("/formation")
+    @GetMapping("/")
     public String formationList(Model model) {
         List<Formation> formations = formationService.getAll();
         model.addAttribute("formations", formations);
         return "showFormation";
     }
 
-    @GetMapping("/formation-form")
+    @GetMapping("/")
     public String formationForm(Model model) {
         Formation formation = new Formation();
         model.addAttribute("formation", formation);
         return "formFormation";
     }
 
-    @PostMapping("/add-formation")
+    @PostMapping("/")
     public String addAddress(@ModelAttribute Formation formation) {
         formationService.saveFormation(formation);
         return "redirect:/center";
     }
 
-    @GetMapping("/formationDetail")
-    public String getFormationById(Model model , @RequestParam Long id) {
+    @GetMapping("/{id}")
+    public String getFormationById(Model model, @RequestParam Long id) {
         Formation formation = formationService.getFormationById(id);
-        model.addAttribute("formations" , formation);
+        model.addAttribute("formations", formation);
         return "showFormationId";
     }
 
+    @PutMapping("/{id}")
+    public Formation updateFormation(@PathVariable Long id, @RequestBody Formation formation) {
+         Formation current = formationService.updateFormationById(formation,id);
+         return  current;
+    }
 }
