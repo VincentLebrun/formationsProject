6657a7608bf4659ad65334fa6f25041501d70a82
vincent <lebrunvincent12@gmail.com>
2022-05-16 09:42:05 +0200

    Modify  base crud in to formation controller remove jsp

--- src/main/java/fr/afpa/formations/controller/FormationController.java
+++ src/main/java/fr/afpa/formations/controller/FormationController.java
@@ -3,48 +3,34 @@ package fr.afpa.formations.controller;
 import fr.afpa.formations.model.Formation;
 import fr.afpa.formations.service.FormationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/formation")
 public class FormationController {
     private final FormationService formationService;
 
     @GetMapping("/")
-    public String formationList(Model model) {
-        List<Formation> formations = formationService.getAll();
-        model.addAttribute("formations", formations);
-        return "showFormation";
-    }
-
-    @GetMapping("/")
-    public String formationForm(Model model) {
-        Formation formation = new Formation();
-        model.addAttribute("formation", formation);
-        return "formFormation";
+    List<Formation> formationList() {
+        return formationService.getAllFormation();
     }
 
     @PostMapping("/")
-    public String addAddress(@ModelAttribute Formation formation) {
-        formationService.saveFormation(formation);
-        return "redirect:/center";
+    public Formation addAddress(@RequestBody Formation formation) {
+        return formationService.saveFormation(formation);
     }
 
-    @GetMapping("/{id}")
-    public String getFormationById(Model model, @RequestParam Long id) {
-        Formation formation = formationService.getFormationById(id);
-        model.addAttribute("formations", formation);
-        return "showFormationId";
+    @DeleteMapping("/{id}")
+    public void deleteFormation(@PathVariable Long id, @RequestBody Formation formation) {
+        formationService.deleteFormationById(id);
     }
 
     @PutMapping("/{id}")
     public Formation updateFormation(@PathVariable Long id, @RequestBody Formation formation) {
-         Formation current = formationService.updateFormationById(formation,id);
-         return  current;
+        Formation current = formationService.updateFormationById(formation, id);
+        return current;
     }
 }
